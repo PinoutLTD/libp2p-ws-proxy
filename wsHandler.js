3ea5846db484574eb70cf5288fc2b9613bb0206a
tubleronchik <m.bystramovich@gmail.com>
2023-11-08 12:38:28 +0100

    add port for ws

--- wsHandler.js
+++ wsHandler.js
@@ -1,8 +1,9 @@
 import { WebSocketServer } from 'ws';
 
 const createWebsocketServer = () => {
-    const wss = new WebSocketServer({ port: 8080 }, () => {
-        console.log('WebSocket server listening on port 8080');
+    const port = 8080
+    const wss = new WebSocketServer({ port: port }, () => {
+        console.log(`WebSocket server listening on port ${port}`);
     });
     return wss 
 }
